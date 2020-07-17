09fc6ca8797fe534db2455bb403e325ea069f93a
bekirytm <bekir.yetim.06@gmail.com>
2020-07-17 18:01:50 +0300

    newUser

--- public/javascripts/controller/indexController.js
+++ public/javascripts/controller/indexController.js
@@ -1,15 +1,29 @@
 app.controller('indexController' , ['$scope' , 'indexFactory' , ($scope , indexFactory) => {
 
-    const connectionOptions = {
-        reconnectionAttempts: 3,
-        reconnectionDelay: 500
+    $scope.init = () => {       //kullanıcıdan isim alma ve bağlantıyı sağlama
+        const username = prompt('please enter username');
+
+        if(username){
+            initSocket(username);
+        }else{
+            return false;
+        }
     }
 
-    indexFactory.connectSocket('http://localhost:3000' , connectionOptions)
-        .then((socket) => {
-            console.log("Bağlantı Gerçekleşti." , socket);
-        }).catch((err) => {
+    function initSocket(username) {
+
+        const connectionOptions = {
+            reconnectionAttempts: 3,
+            reconnectionDelay: 500
+        }
+
+        indexFactory.connectSocket('http://localhost:3000' , connectionOptions)
+            .then((socket) => {
+                socket.emit('newUser' , { username });
+            }).catch((err) => {
             console.log(err);
         });
+
+    }
 
 }]);
--- src/socketApi.js
+++ src/socketApi.js
@@ -2,12 +2,26 @@ const socketio = require("socket.io");
 const io = socketio();
 
 const socketApi = { };
-
 socketApi.io = io;
+
+const users = [ ];
 
 io.on('connection' , (socket) => {
     console.log("Kullanıcı Bağlandı.");
 
+    socket.on('newUser' , (data) => {
+        const defaultData = {               //Varsayılan kullanıcı bilgileri
+            id: socket.id,
+            position: {
+                x: 0,
+                y: 0
+            }
+        }
+
+        const userData = Object.assign(data,defaultData);
+        users.push(userData);
+        console.log(users);
+    });
 });
 
 
