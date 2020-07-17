40fa2446f7898d1fc1beef076dd6a4fdd3e48b90
bekirytm <bekir.yetim.06@gmail.com>
2020-07-17 13:15:16 +0300

    index Factory

--- public/javascripts/controller/indexController.js
+++ public/javascripts/controller/indexController.js
@@ -1,3 +1,15 @@
-app.controller('indexController' , ['$scope' , ($scope) => {
-    const socket = io.connect('http://localhost:3000');
+app.controller('indexController' , ['$scope' , 'indexFactory' , ($scope , indexFactory) => {
+
+    const connectionOptions = {
+        reconnectionAttempts: 3,
+        reconnectionDelay: 500
+    }
+
+    indexFactory.connectSocket('http://localhost:3000' , connectionOptions)
+        .then((socket) => {
+            console.log("Bağlantı Gerçekleşti." , socket);
+        }).catch((err) => {
+            console.log(err);
+        });
+
 }]);
